dcbf9aac042303ded28bc8bf6f3fda0c4ac2ec0b
Santosh Kumar Upadhyay <er.santosh.upadhyay@gmail.com>
2025-02-01 18:05:50 +0530

    Add user controllers and user route

---
+++ backend/controllers/productController.js
@@ -0,0 +1,19 @@
+import { v2 as cloudinary } from 'cloudinary'
+import productModel from '../models/productModel.js'
+
+// function for add product 
+const addProduct = async (req, res) => {
+
+}
+// function for list product 
+const listProduct = async (req, res) => {
+    
+}
+// function for remove product 
+const removeProduct = async (req, res) => {
+    
+}
+// function for singl product Info
+const singleProduct = async (req, res) => {
+    
+}
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from "./config/mongodb.js";
 import connectCoudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
 
 //app config
 const app = express()
@@ -19,6 +20,7 @@ app.use(express.json())
 app.use(cors());
 
 // api endpoint
+app.use('/api/user',userRouter)
 
 app.get('/',(req,res)=>{
     res.send("API WORKING")
