2edff599ccd94a6cc815e5c8c8386325bfae8913
Santosh Kumar Upadhyay <er.santosh.upadhyay@gmail.com>
2025-02-10 18:33:19 +0530

    add logo

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import {assets} from '../assets/admin_assets/assets'
 const Navbar = ({setToken}) => {
   return (
     <div className='flex items-center justify-between py-2 px-[4%]'>
-      <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
+      <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />Admin Panel
       <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full' >Logout</button>
     </div>
   );
